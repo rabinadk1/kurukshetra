1962e8d90554908c9581ca77a5bce75752107b5e
Maverick <rabin.adk1@gmail.com>
2019-02-12 22:35:20 +0545

    Initial base for Kurukshetra

---
+++ Game.cpp
@@ -0,0 +1,86 @@
+//
+// Created by maverick on 12/2/19.
+//
+
+#include "Game.h"
+#include <iostream>
+Game::Game()
+    : mWindow(sf::VideoMode(640, 480), "SFML Application"),
+    mIsMovingUp(false),
+    mIsMovingDown(false),
+    mIsMovingLeft(false),
+    mIsMovingRight(false)
+    {
+        mWindow.setVerticalSyncEnabled(true);
+        if (not mTexture.loadFromFile("../Eagle.gif")) {
+            std::cerr<<"Couldn't load texture from the file";
+            exit(1);
+        }
+        mTexture.setSmooth(true);
+        mSprite.setTexture(mTexture);
+        mSprite.setPosition(100.f, 100.f);
+    }
+
+void Game::run() {
+    while (mWindow.isOpen())
+    {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void Game::processEvents() {
+    sf::Event event = {};
+    while (mWindow.pollEvent(event))
+    {
+        switch (event.type) {
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+        }
+    }
+}
+
+void Game::update() {
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= 1.f;
+    if (mIsMovingDown)
+        movement.y += 1.f;
+    if (mIsMovingLeft)
+        movement.x -= 1.f;
+    if (mIsMovingRight)
+        movement.x += 1.f;
+    mSprite.move(movement);
+}
+
+void Game::render() {
+    mWindow.clear();
+    mWindow.draw(mSprite);
+    mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+    switch (key)
+    {
+        case sf::Keyboard::W:
+            mIsMovingUp = isPressed;
+            break;
+        case sf::Keyboard::A:
+            mIsMovingLeft = isPressed;
+            break;
+        case sf::Keyboard::S:
+            mIsMovingDown = isPressed;
+            break;
+        case sf::Keyboard::D:
+            mIsMovingRight = isPressed;
+            break;
+    }
+}
---
+++ Game.h
@@ -0,0 +1,25 @@
+//
+// Created by maverick on 12/2/19.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Game {
+public:
+    Game();
+    void run();
+
+private:
+    void processEvents();
+    void update();
+    void render();
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+private:
+    sf::RenderWindow mWindow;
+    sf::Sprite mSprite;
+    sf::Texture mTexture;
+    bool mIsMovingUp, mIsMovingDown, mIsMovingLeft, mIsMovingRight;
+};
