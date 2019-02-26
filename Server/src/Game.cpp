29b3c48360ab4de9760e4c903da70ec6852f6bee
safalthapaliya <safalthapaliya@gmail.com>
2019-02-26 10:04:53 +0545

    enemy also appears

--- Server/src/Game.cpp
+++ Server/src/Game.cpp
@@ -87,7 +87,10 @@ void Game::update() {
     window.setView(gameView);
     float elapsedTime = clock.restart().asSeconds();
     if(server.getM_playersConnected()>0)
-    player.Update( &textures.get(Textures::bulletTexture), elapsedTime, gameView, baseHeight, window, server);
+	{
+    	player.Update( &textures.get(Textures::bulletTexture), elapsedTime, gameView, baseHeight, window, server);
+		enemy.Update( &textures.get(Textures::bulletTexture), elapsedTime, gameView, baseHeight, window);
+	}
     else
     	player.Update( &textures.get(Textures::bulletTexture), elapsedTime, gameView, baseHeight, window);
    // enemy.Update( &textures.get(Textures::bulletTexture), elapsedTime, gameView, baseHeight, window);
